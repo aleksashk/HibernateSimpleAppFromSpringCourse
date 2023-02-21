c06508e4dc341f448cfa8a386cc4f413ee73535e
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-21 10:52:28 +0300

    OneToOne get details about employee with Cascade.ALL

--- src/main/java/ru/philimonov/hibernate_one_to_one/Starter.java
+++ src/main/java/ru/philimonov/hibernate_one_to_one/Starter.java
@@ -9,13 +9,19 @@ import ru.philimonov.hibernate_one_to_one.entity.Employee;
 public class Starter {
     public static void main(String[] args) {
         try(SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Employee.class).addAnnotatedClass(Detail.class).buildSessionFactory()){
-            Employee employee = new Employee("Aleksandr", "Philimonov", "IT", 500);
-            Detail detail = new Detail("Minsk", "457-898-745", "flameksandr@gmail.com");
-            employee.setEmpDetail(detail);
+//            Employee employee = new Employee("Boris", "Jonson", "HR", 300);
+//            Detail detail = new Detail("London", "025-54-89", "jonson@gmail.com");
+//            employee.setEmpDetail(detail);
+//            Session session = factory.getCurrentSession();
+//            session.beginTransaction();
+//
+//            session.save(employee);
+
             Session session = factory.getCurrentSession();
             session.beginTransaction();
 
-            session.save(employee);
+            Employee employee = session.get(Employee.class, 1);
+            System.out.println(employee.getEmpDetail());
 
             session.getTransaction().commit();
             System.out.println("Done!");
