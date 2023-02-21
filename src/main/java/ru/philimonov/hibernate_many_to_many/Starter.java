7858e80d0c90c67bbb925ea82da74e7aa02eab6d
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-21 13:30:11 +0300

    ManyToMany add section to the children

--- src/main/java/ru/philimonov/hibernate_many_to_many/Starter.java
+++ src/main/java/ru/philimonov/hibernate_many_to_many/Starter.java
@@ -8,22 +8,29 @@ import ru.philimonov.hibernate_many_to_many.entity.Section;
 
 public class Starter {
     public static void main(String[] args) {
-        try(SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Child.class).addAnnotatedClass(Section.class).buildSessionFactory(); Session session = factory.getCurrentSession()){
+        try (SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Child.class).addAnnotatedClass(Section.class).buildSessionFactory(); Session session = factory.getCurrentSession()) {
 
+//            Section section = new Section("Footbal");
+//
+//            Child child1 = new Child("Aleks", 5);
+//            Child child2 = new Child("Lena", 6);
+//            Child child3 = new Child("Dasha", 4);
+//
+//            section.addChildToSection(child1);
+//            section.addChildToSection(child2);
+//            section.addChildToSection(child3);
+//************************************************************************************
+            Section section1 = new Section("Volleyball");
+            Section section2 = new Section("Karate");
+            Section section3 = new Section("Hockey");
 
-            Section section = new Section("Footbal");
-
-            Child child1 = new Child("Aleks", 5);
-            Child child2 = new Child("Lena", 6);
-            Child child3 = new Child("Dasha", 4);
-
-            section.addChildToSection(child1);
-            section.addChildToSection(child2);
-            section.addChildToSection(child3);
-
+            Child child = new Child("Roma", 9);
+            child.addSectionToChild(section1);
+            child.addSectionToChild(section2);
+            child.addSectionToChild(section3);
             session.beginTransaction();
 
-            session.save(section);
+            session.save(child);
 
             session.getTransaction().commit();
             System.out.println("Done!!!");
